44ab97b8c428c316516e71f43ae27020b9eb1b04
Santiago Montoya Velez <dejesus.montoya@est.iudigital.edu.co>
2025-12-15 00:36:48 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from turtle_class import Tortuga
+
+# Crear dos tortugas independientes
+t1 = Tortuga()
+t2 = Tortuga()
+
+print("Tortuga 1:")
+t1.adelante(10)
+t1.abajo(3)
+t1.adelante(5)
+t1.mostrar()
+
+print("\nTortuga 2:")
+t2.adelante(5)
+t2.abajo(2)
+t2.adelante(8)
+t2.mostrar()
+
